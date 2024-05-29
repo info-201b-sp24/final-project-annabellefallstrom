cfff4c9378a0782a7171634540c53343581088a4
ryan1kang03 <165976560+ryan1kang03@users.noreply.github.com>
2024-05-29 16:07:31 -0700

    Update chart1.R

--- chart1.R
+++ chart1.R
@@ -4,11 +4,7 @@ library(readxl)
 library(ggplot2)
 library(plotly)
 
-ui <- fluidPage(
-  titlePanel("Top 7 Countries with Highest CO2 Emissions (CDP)"),
-  plotlyOutput("emissionsPlot")
-)
-
+# Define the server logic
 server <- function(input, output) {
   emissions_data <- reactive({
     file_path <- "./D_FINAL.xlsx"
@@ -18,16 +14,17 @@ server <- function(input, output) {
              Total_Emissions_CDP = `Total emissions (CDP) [tCO2-eq]`, 
              Country = Country) %>%
       arrange(desc(Total_Emissions_CDP)) %>%
-      head(10)
+      head(7)  # Adjusted to show the top 7 countries
     data
   })
   
   # Render the plot
   output$emissionsPlot <- renderPlotly({
-    gg_chart <- ggplot(emissions_data(), aes(x = reorder(Country, -Total_Emissions_CDP), y = Total_Emissions_CDP, fill = Country)) +
+    selected_emission <- input$emissions_selector
+    gg_chart <- ggplot(emissions_data(), aes_string(x = "Country", y = selected_emission, fill = "Country")) +
       geom_bar(stat = "identity") +
       scale_fill_manual(values = rep(c("darkseagreen2", "darkseagreen", "darkolivegreen", "darkgreen", "black"), 2)) +
-      labs(x = "Country", y = "Total Emissions (CDP) [tCO2-eq]", title = "Top 7 Countries with Highest CO2 Emissions (CDP)") +
+      labs(x = "Country", y = selected_emission, title = "Top 7 Countries with Highest CO2 Emissions (CDP)") +
       theme_minimal()
     ggplotly(gg_chart)
   })
